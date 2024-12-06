9149af5675d0e3949cab443fd6e3bb0a0112b7ff
joaquincodigo <joaquin.codigo@gmail.com>
2024-12-06 18:27:15 -0300

    converted SearchBar to a client component

--- app/page.js
+++ app/page.js
@@ -19,7 +19,6 @@ export default function Home() {
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold mb-4">Recipe Manager</h1>
-      <SearchBar/>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {recipes.map((recipe) => (
           <div key={recipe.id} className="p-4 border rounded-lg">
