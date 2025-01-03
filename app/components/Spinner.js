4c1925763dce80480edd5cfcabf40b895dc0da4a
joaquincodigo <joaquin.codigo@gmail.com>
2025-01-03 12:46:11 -0300

    hand made spinner

---
+++ app/components/Spinner.js
@@ -0,0 +1,50 @@
+// components/Spinner.jsx
+export default function Spinner() {
+  return (
+    // OUTER CONTAINER
+    <div className="relative flex">
+      {/* LAYER 1 CONTAINER */}
+
+      <div className="absolute w-16 h-16 flex flex-col animate-spin">
+        {/* LINE */}
+        <div className="h-1/3 w-full flex items-center justify-center">
+          <div className="h-3 w-3 bg-slate-400 rounded-full"></div>
+        </div>
+
+        {/* LINE */}
+        <div className="h-1/3 w-full flex items-center justify-around">
+          <div className="h-3 w-3 bg-slate-400 rounded-full"></div>
+          <div className="h-3 w-3 bg-transparent rounded-full"></div>
+          <div className="h-3 w-3 bg-slate-400 rounded-full"></div>
+        </div>
+
+        {/* LINE */}
+        <div className="h-1/3 w-full flex items-center justify-center">
+          <div className="h-3 w-3 bg-slate-400 rounded-full"></div>
+        </div>
+      </div>
+
+      {/* LAYER 2 CONTAINER */}
+      <div className="flex absolute rotate-45">
+        <div className="w-16 h-16 flex flex-col animate-spin">
+          {/* LINE */}
+          <div className="h-1/3 w-full flex items-center justify-center">
+            <div className="h-3 w-3 bg-slate-400 rounded-full"></div> 	
+          </div>
+
+          {/* LINE */}
+          <div className="h-1/3 w-full flex items-center justify-around">
+            <div className="h-3 w-3 bg-slate-400 rounded-full"></div>
+            <div className="h-3 w-3 bg-transparent rounded-full"></div>
+            <div className="h-3 w-3 bg-slate-400 rounded-full"></div>
+          </div>
+
+          {/* LINE */}
+          <div className="h-1/3 w-full flex items-center justify-center">
+            <div className="h-3 w-3 bg-slate-400 rounded-full"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/recipe/[id]/page.js
+++ app/recipe/[id]/page.js
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from "react";
 import { fetchRecipeById } from "../../../utils/fetchRecipes";
+import Spinner from "../../components/Spinner";
+
 import { IoIosTimer } from "react-icons/io";
 import { RiRestaurantLine } from "react-icons/ri";
 import { GrRestaurant } from "react-icons/gr";
@@ -32,108 +34,117 @@ export default function RecipePage({ params }) {
     fetchData();
   }, [params]);
 
-  if (loading) return <p>Loading...</p>; // TODO SPINNER
+  if (loading)
+    return (
+      <>
+        <p>Hello</p>
+        <Spinner />
+      </>
+    );
+
   if (error) return <p>An error has ocurred</p>; // TODO ERROR ICON
 
   return (
-    <div className="p-3 pb-10">
-      {/* TITLE AND PREVIEW */}
-      <section className="mb-12">
-        <img
-          className="w-full rounded-lg mb-3"
-          src={recipe?.image}
-          alt="The finished recipe"
-        />
-        <h1 className="font-bold text-4xl mb-3">
-          {recipe?.title.charAt(0).toUpperCase() +
-            recipe?.title.slice(1).toLowerCase()}
-        </h1>
-        <p>{recipe?.description}</p>
-      </section>
 
-      {/* PREPARATION TIME */}
-      <section className="mb-6">
-        <h2 className="font-bold text-xl mb-3">Preparation time</h2>
-        <ul className="flex items-center">
-          <IoIosTimer className="text-2xl font-extrabold text-slate-500 mr-1" />
-          <li>{recipe?.preparation_time} minutes</li>
-        </ul>
-      </section>
+        <Spinner />
+    // <div className="p-3 pb-10">
+    //   {/* TITLE AND PREVIEW */}
+    //   <section className="mb-12">
+    //     <img
+    //       className="w-full rounded-lg mb-3"
+    //       src={recipe?.image}
+    //       alt="The finished recipe"
+    //     />
+    //     <h1 className="font-bold text-4xl mb-3">
+    //       {recipe?.title.charAt(0).toUpperCase() +
+    //         recipe?.title.slice(1).toLowerCase()}
+    //     </h1>
+    //     <p>{recipe?.description}</p>
+    //   </section>
 
-      {/* INGREDIENTS */}
-      <section className="mb-6">
-        <h2 className="mb-3 text-xl font-bold">Ingredients</h2>
-        <ul className="mb-3">
-          {recipe?.ingredients?.map((ingredient, index) => (
-            <li className="ms-5 list-disc" key={index}>
-              {ingredient}
-            </li>
-          ))}
-        </ul>
-      </section>
+    //   {/* PREPARATION TIME */}
+    //   <section className="mb-6">
+    //     <h2 className="font-bold text-xl mb-3">Preparation time</h2>
+    //     <ul className="flex items-center">
+    //       <IoIosTimer className="text-2xl font-extrabold text-slate-500 mr-1" />
+    //       <li>{recipe?.preparation_time} minutes</li>
+    //     </ul>
+    //   </section>
 
-      {/* PREPARATIONS STEPS  */}
-      <section className="mb-6">
-        <h2 className="font-bold text-xl mb-3">Preparation Steps</h2>
-        <ul className=" relative">
-          <div className="absolute left-[14px] top-0 h-full w-1 bg-[#dddddd] z-[0]"></div>
+    //   {/* INGREDIENTS */}
+    //   <section className="mb-6">
+    //     <h2 className="mb-3 text-xl font-bold">Ingredients</h2>
+    //     <ul className="mb-3">
+    //       {recipe?.ingredients?.map((ingredient, index) => (
+    //         <li className="ms-5 list-disc" key={index}>
+    //           {ingredient}
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </section>
 
-          {recipe.preparation_steps.map((step, index) => (
-            <li className="flex items-center mb-3" key={index}>
-              <div
-                className="
-                    z-10
-                    flex
-                    items-center
-                    justify-center
-                    h-8
-                    w-8
-                    rounded-full
-                    border-solid
-                    border-4
-                    border-[#dddddd]
-                    p-3
-                    font-bold
-                    mr-3
-                    bg-white
-                    text-slate-500
-                  "
-              >
-                {index + 1}
-              </div>
+    //   {/* PREPARATIONS STEPS  */}
+    //   <section className="mb-6">
+    //     <h2 className="font-bold text-xl mb-3">Preparation Steps</h2>
+    //     <ul className=" relative">
+    //       <div className="absolute left-[14px] top-0 h-full w-1 bg-[#dddddd] z-[0]"></div>
 
-              {recipe.preparation_steps[index]}
-            </li>
-          ))}
-        </ul>
-      </section>
+    //       {recipe.preparation_steps.map((step, index) => (
+    //         <li className="flex items-center mb-3" key={index}>
+    //           <div
+    //             className="
+    //                 z-10
+    //                 flex
+    //                 items-center
+    //                 justify-center
+    //                 h-8
+    //                 w-8
+    //                 rounded-full
+    //                 border-solid
+    //                 border-4
+    //                 border-[#dddddd]
+    //                 p-3
+    //                 font-bold
+    //                 mr-3
+    //                 bg-white
+    //                 text-slate-500
+    //               "
+    //           >
+    //             {index + 1}
+    //           </div>
 
-      {/* SERVINGS */}
-      <section className="mb-6">
-        <h2 className="font-bold text-xl mb-3">Yield</h2>
-        <ul>
-          <li className="flex items-center">
-            <RiRestaurantLine className="text-2xl font-extrabold text-slate-500 mr-1" />
-            {recipe?.servings} servings
-          </li>
-        </ul>
-      </section>
+    //           {recipe.preparation_steps[index]}
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </section>
 
-      {/* CATEGORY */}
-      <section className="mb-6">
-        <h2 className="font-bold text-xl mb-3">Category</h2>
-        <ul>
-          <li className="ms-5 list-disc">{recipe?.category}</li>
-        </ul>
-      </section>
+    //   {/* SERVINGS */}
+    //   <section className="mb-6">
+    //     <h2 className="font-bold text-xl mb-3">Yield</h2>
+    //     <ul>
+    //       <li className="flex items-center">
+    //         <RiRestaurantLine className="text-2xl font-extrabold text-slate-500 mr-1" />
+    //         {recipe?.servings} servings
+    //       </li>
+    //     </ul>
+    //   </section>
 
-      {/* DIFFICULTY */}
-      <section className="mb-6">
-        <h2 className="font-bold text-xl mb-3">Difficulty</h2>
-        <ul>
-          <li className="ms-5 list-disc">{recipe?.difficulty}</li>
-        </ul>
-      </section>
-    </div>
+    //   {/* CATEGORY */}
+    //   <section className="mb-6">
+    //     <h2 className="font-bold text-xl mb-3">Category</h2>
+    //     <ul>
+    //       <li className="ms-5 list-disc">{recipe?.category}</li>
+    //     </ul>
+    //   </section>
+
+    //   {/* DIFFICULTY */}
+    //   <section className="mb-6">
+    //     <h2 className="font-bold text-xl mb-3">Difficulty</h2>
+    //     <ul>
+    //       <li className="ms-5 list-disc">{recipe?.difficulty}</li>
+    //     </ul>
+    //   </section>
+    // </div>
   );
 }
