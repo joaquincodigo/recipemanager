a6ffa9ba1dd0494675f2e3d8182861d19467a34e
joaquincodigo <joaquin.codigo@gmail.com>
2025-01-17 18:31:04 -0300

    grouped pages

---
+++ app/(pages)/favorite-list/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FavoritesListPage() {
+	return (
+    <div className="p-3">
+      <h1 className="text-xl font-bold">Favorites List Page</h1>
+    </div>
+	)
+}
