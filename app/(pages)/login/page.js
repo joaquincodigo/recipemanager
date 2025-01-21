ec86779ead8751acaf0861f0c007b72b87436b05
joaquincodigo <joaquin.codigo@gmail.com>
2025-01-21 12:45:26 -0300

    login page

---
+++ app/(pages)/login/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function LoginPage() {
+  return (
+    <div className="p-3">
+      <h1 className="text-xl font-bold">Login</h1>
+    </div>
+  );
+}
+
--- app/(pages)/my-recipes/page.js
+++ app/(pages)/my-recipes/page.js
@@ -1,34 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
-import { useAuth } from "../../context/AuthContext";
-
 export default function MyRecipesPage() {
-  const { user, signIn } = useAuth();
-
-  useEffect(() => {
-    const signInUser = async () => {
-      try {
-        const { user, error } = await signIn({
-          email: "user@example.com",
-          password: "12345",
-        });
-
-        if (error) {
-          console.error("Sign-in error:", error.message);
-        } else {
-          console.log("User signed in:", user);
-        }
-      } catch (err) {
-        console.error("Unexpected error:", err.message);
-      }
-    };
-
-    if (!user) {
-      signInUser();
-    }
-  }, [signIn, user]);
-
   return (
     <div className="p-3">
       <h1 className="text-xl font-bold">Your Recipes</h1>
