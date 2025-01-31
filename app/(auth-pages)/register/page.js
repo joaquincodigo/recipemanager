f47dbd6a7a9e5eeec0f99e949ba54bb09b10c47a
joaquincodigo <joaquin.codigo@gmail.com>
2025-01-31 13:33:21 -0300

    More design of the register form

--- app/(auth-pages)/register/page.js
+++ app/(auth-pages)/register/page.js
@@ -2,7 +2,7 @@ import { PauseCircleIcon } from "@heroicons/react/24/solid";
 
 export default function RegisterPage() {
   return (
-    <form className="p-3 flex flex-col gap-y-8">
+    <form className="p-3 flex flex-col gap-y-8" autoComplete="off">
       {/* +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-*/}
       {/* TODO: ADD LOGO AFTER YOU FINISH IT  */}
       {/* +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-*/}
@@ -20,7 +20,7 @@ export default function RegisterPage() {
           Name *
         </label>
         <input
-          className="bg-white p-3 rounded-md border focus:outline-none focus:ring-2 focus:ring-[#7FC37E]"
+          className="bg-white p-3 rounded-md border  border-slate-400 focus:outline-none focus:ring-2 focus:ring-[#7FC37E]"
           placeholder="Enter your name"
           type="text"
           id="name"
@@ -39,7 +39,7 @@ export default function RegisterPage() {
           Surname
         </label>
         <input
-          className="bg-white p-3 rounded-md border focus:outline-none focus:ring-2 focus:ring-[#7FC37E]"
+          className="bg-white p-3 rounded-md border border-slate-400 focus:outline-none focus:ring-2 focus:ring-[#7FC37E]"
           placeholder="Enter your surname"
           type="text"
           id="surname"
@@ -57,12 +57,12 @@ export default function RegisterPage() {
           Mail *
         </label>
         <input
-          className="bg-white p-3 rounded-md border focus:outline-none focus:ring-2 focus:ring-[#7FC37E]"
+          className="bg-white p-3 rounded-md border border-slate-400 focus:outline-none focus:ring-2 focus:ring-[#7FC37E]"
           placeholder="Enter your mail"
           type="email"
           id="email"
-          name="email"
-          autoComplete="off"
+          name="email"// MDN Docs says that if the browser still autocompletes, then I have to change the name of the input.
+          autoComplete="new-mail"
         />
       </div>
 
@@ -75,20 +75,18 @@ export default function RegisterPage() {
           Password *
         </label>
         <input
-          className="bg-white p-3 rounded-md border focus:outline-none focus:ring-2 focus:ring-[#7FC37E]"
+          className="bg-white p-3 rounded-md border border-slate-400 focus:outline-none focus:ring-2 focus:ring-[#7FC37E]"
           placeholder="Enter your mail"
           type="password"
           id="password"
           name="password"
-          autoComplete="off"
+          autoComplete="new-password"
         />
       </div>
 
-			
-
       {/* REGISTER BUTTON */}
       <button className="p-3 text-white font-bold rounded-md bg-[#7FC37E]">
-				Sign Up
+        Sign Up
       </button>
     </form>
   );
