88b96ca0781bb6fbbd862f05c7fe6082ff9da1b1
joaquincodigo <joaquin.codigo@gmail.com>
2025-02-03 17:42:42 -0300

    Added autofocus handling.

--- app/(auth-pages)/register/page.js
+++ app/(auth-pages)/register/page.js
@@ -1,16 +1,21 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { PauseCircleIcon } from "@heroicons/react/24/solid";
 
 export default function RegisterPage() {
-  const [name, setName] = useState(null);
-  const [lastname, setLastname] = useState(null);
-  const [mail, setMail] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [passwordVerification, setPasswordVerification] = useState(null);
+  const [formData, setFormData] = useState({
+    name: "",
+    lastname: "",
+    email: "",
+    password: "",
+    passwordVerification: "",
+  });
 
   const [currentPage, setCurrentPage] = useState(1);
+  const nameRef = useRef(null)
+  const emailRef = useRef(null)
+  const passwordRef = useRef(null)
 
   const handleNext = (e) => {
     e.preventDefault();
@@ -19,11 +24,33 @@ export default function RegisterPage() {
     }
   };
 
+  // Auto-focus handling 
+  useEffect(() => {
+    if (currentPage === 1) {
+      nameRef.current?.focus();
+    }
+    if (currentPage === 2) {
+      emailRef.current?.focus();
+    }
+    if (currentPage === 3) {
+      passwordRef.current?.focus();
+    }
+  }, [currentPage]); 
+
+
   const handleBack = (e) => {
     e.preventDefault();
     if (currentPage === 2 || currentPage === 3) {
       setCurrentPage((prev) => prev - 1);
     }
+  };
+
+  const isValidName = () => {
+    return formData.name.length >= 2;
+  };
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
   return (
@@ -55,6 +82,9 @@ export default function RegisterPage() {
                 name="name"
                 autoComplete="off"
                 required
+                ref={nameRef}
+                onChange={handleChange}
+                value={formData.name}
               />
             </div>
             {/* LASTNAME */}
@@ -72,6 +102,8 @@ export default function RegisterPage() {
                 id="lastname"
                 name="lastname"
                 autoComplete="off"
+                onChange={handleChange}
+                value={formData.lastname}
               />
             </div>
           </>
@@ -95,6 +127,9 @@ export default function RegisterPage() {
                 id="email"
                 name="email"
                 autoComplete="new-mail"
+                onChange={handleChange}
+                value={formData.email}
+                ref={emailRef}
               />
             </div>
           </>
@@ -118,6 +153,9 @@ export default function RegisterPage() {
                 id="password"
                 name="password"
                 autoComplete="new-password"
+                value={formData.password}
+                onChange={handleChange}
+                ref={passwordRef}
               />
             </div>
 
@@ -133,9 +171,11 @@ export default function RegisterPage() {
                 className="bg-white w-64 p-3 rounded-md border border-slate-400 focus:outline-none focus:ring-2 focus:ring-[#7FC37E]"
                 placeholder="Re-enter your password"
                 type="password"
-                id="password-verfication"
-                name="password-verification"
-                autoComplete="password-verification"
+                id="passwordVerfication"
+                name="passwordVerification"
+                autoComplete="passwordVerification"
+                onChange={handleChange}
+                value={formData.passwordVerification}
               />
             </div>
           </>
@@ -155,8 +195,18 @@ export default function RegisterPage() {
           )}
 
           <button
+            disabled={!isValidName()}
             onClick={handleNext}
-            className="p-3 flex-[2] text-white font-bold rounded-md bg-[#7FC37E]"
+            className={`
+            p-3
+            flex-[2]
+            font-bold
+            rounded-md
+            ${
+              isValidName()
+                ? "bg-[#7FC37E] text-white"
+                : "bg-gray-400 text-gray-200"
+            }`}
           >
             Next
           </button>
