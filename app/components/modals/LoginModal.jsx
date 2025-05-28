ecfe672445791ea0b1bd453cc8094bf289225e38
joaquincodigo <joaquin.codigo@gmail.com>
2025-05-28 10:58:33 -0300

    Added copy buttons for modal credentials

--- app/components/modals/LoginModal.jsx
+++ app/components/modals/LoginModal.jsx
@@ -1,13 +1,26 @@
 import Link from "next/link";
 import Modal, { styles } from "./Modal";
 import useViewport from "@/app/hooks/useViewport";
+import { Square2StackIcon } from "@heroicons/react/24/outline";
+
+const localStyles = {
+  copyIcon:
+    "w-5 h-5 border text-slate-400 border-slate-300 rounded-md hover:bg-slate-200",
+  credential: "flex items-center gap-2 w-max ps-3 ",
+  credentialsList: "mt-3",
+  listDecorator: "text-black select-none", // best way to have flex aligment and the decorator. Other options, like using a wrapper div, created a lot of problems.
+};
 
 export default function LoginModal() {
   const isMobile = useViewport();
 
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+  };
+
   return (
     <Modal
-      windowTitle="Logging-in..."
+      windowTitle="Logging-in"
       xOffset={isMobile ? "5%" : "15%"}
       yOffset={isMobile ? "2%" : "5%"}
     >
@@ -23,12 +36,30 @@ export default function LoginModal() {
 
       <p>Or try the app using demo credentials:</p>
 
-      <ul>
-        <li className={styles.credential}>
-          Email: <span className={styles.bold}>user@mail.com</span>
+      <ul className={localStyles.credentialsList}>
+        <li className={localStyles.credential}>
+          <span className={localStyles.listDecorator}>•</span>
+          Email:
+          <span className={styles.bold}>user@mail.com</span>
+          <button
+            onClick={() => {
+              copyToClipboard("user@mail.com");
+            }}
+          >
+            <Square2StackIcon className={localStyles.copyIcon} />
+          </button>
         </li>
-        <li className={styles.credential}>
-          Password: <span className={styles.bold}>123456</span>
+        <li className={localStyles.credential}>
+          <span className={localStyles.listDecorator}>•</span>
+          Password:
+          <span className={styles.bold}>123456</span>
+          <button
+            onClick={() => {
+              copyToClipboard("123456");
+            }}
+          >
+            <Square2StackIcon className={localStyles.copyIcon} />
+          </button>
         </li>
       </ul>
     </Modal>
--- app/components/modals/Modal.jsx
+++ app/components/modals/Modal.jsx
@@ -6,14 +6,13 @@ import Button from "../ui/Button";
 
 export const styles = {
   window:
-    "flex flex-col fixed z-50 min-w-52 max-w-64 rounded-md shadow-lg  backdrop-blur-sm",
+    "flex flex-col fixed z-50 min-w-52 max-w-72 rounded-md shadow-lg  backdrop-blur-sm",
   topBar: "bg-[#7FC37E] text-white flex justify-between items-center ps-2 pe-1",
   closeIcon: "w-5 h-5 border-white border-[1.5px] border-black rounded-md",
   content: "p-2",
   footer: "p-2 flex flex-grow items-end justify-end",
   link: "text-[#099107] underline",
   bold: "font-semibold",
-  credential: "w-max ps-3 list-inside list-disc",
 };
 
 export default function Modal({
