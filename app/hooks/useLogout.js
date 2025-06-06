4e1d0fd2a8257fe38ab88a209dd0e818ee595048
joaquincodigo <joaquin.codigo@gmail.com>
2025-06-06 16:35:14 -0300

    Loggin off now re-directs to home.

--- app/hooks/useLogout.js
+++ app/hooks/useLogout.js
@@ -1,7 +1,7 @@
 const useLogout = () => {
   const handleLogout = () => {
     document.cookie = "userId=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-    window.location.reload();
+    window.location.href = "/home";
   };
   return { handleLogout };
 };
